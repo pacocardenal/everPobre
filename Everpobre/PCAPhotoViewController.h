98b3a04afdf834655b59da91f20fcfc016c12bc4
Paco Cardenal <pacocardenal@pacocardenal.com>
2015-02-05 18:14:20 +0100

    Custom Cells + Note View Controller implemented

---
+++ Everpobre/PCAPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCAPhotoViewController.h
+//  Everpobre
+//
+//  Created by Paco on 5/2/15.
+//  Copyright (c) 2015 Paco Cardenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAPhotoViewController : UIViewController
+
+@end
---
+++ Everpobre/PCAPhotoViewController.m
@@ -0,0 +1,37 @@
+//
+//  PCAPhotoViewController.m
+//  Everpobre
+//
+//  Created by Paco on 5/2/15.
+//  Copyright (c) 2015 Paco Cardenal. All rights reserved.
+//
+
+#import "PCAPhotoViewController.h"
+
+@interface PCAPhotoViewController ()
+
+@end
+
+@implementation PCAPhotoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
